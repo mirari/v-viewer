8f4b120d91db9a7e7437c9ff2109741de4f631ec
mirari <clover4@gmail.com>
2020-03-16 23:53:44 +0800

    remove `circle-ci` add `github-actions` auto deploy gh-pages

--- build/webpack.release.js
+++ build/webpack.release.js
@@ -13,6 +13,12 @@ module.exports = {
     filename: './dist/[name].js',
     library: 'VueViewer',
     libraryTarget: 'umd'
+    // src/index.js文件采用export default语法抛出，因此需要设置libraryExport
+    // 否则引入的UI组件库需要使用.default才能引用到抛出的对象
+    // if your entry has a default export of `MyDefaultModule`
+    // var MyDefaultModule = _entry_return_.default;
+    // 这里踩过坑，所以说明一下，不配置的话遇到的问题是引入的UI组件库没法解构
+    // libraryExport: 'default'
   },
   externals: [
     {
